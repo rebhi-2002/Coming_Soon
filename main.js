20603eb47821bba7fe936ae23aca839014309b28
Rebhe <rebheibrahim@gmail.com>
2023-01-05 18:27:10 +0200

    Modified Time

--- main.js
+++ main.js
@@ -3,7 +3,10 @@ function toggle() {
   newsletter.classList.toggle("active");
 }
 
-var countDate = new Date("Jan 1, 2023 00:00:00").getTime();
+/* var countDate = new Date("Jan 1, 2023 00:00:00").getTime(); */
+var countDate = new Date(
+  `Jan 1, ${new Date().getFullYear() + 1} 00:00:00`
+).getTime();
 
 function newYear() {
   var now = new Date().getTime();
